c2106f7bb8e5470406ea82846df9f8b3c8263ff7
Rafael Rodriges <45487272+strongbar@users.noreply.github.com>
2022-04-30 21:44:17 +0100

    Criação titulo, barra de pesquisa, e botão adicionado icon de pesquisa

--- app/src/main/java/ipvc/estg/easyparking/MainActivity.kt
+++ app/src/main/java/ipvc/estg/easyparking/MainActivity.kt
@@ -1,13 +1,24 @@
 package ipvc.estg.easyparking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pesquisa =findViewById<Button>(R.id.btnPesquisa)
+        pesquisa.setOnClickListener{
+            val intent= Intent(this@MainActivity,Pesquisa::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
 
 //Teste Commit
