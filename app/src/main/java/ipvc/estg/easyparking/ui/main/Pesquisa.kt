d8ff0d91b0dac7fd93313b22211681b0e587893b
Rafael Rodriges <45487272+strongbar@users.noreply.github.com>
2022-04-30 20:35:13 +0100

    Criação de activity_pesquisa.xml Sem código, apenas onCreate

---
+++ app/src/main/java/ipvc/estg/easyparking/ui/main/Pesquisa.kt
@@ -0,0 +1,12 @@
+package ipvc.estg.easyparking.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ipvc.estg.easyparking.R
+
+class Pesquisa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pesquisa)
+    }
+}
